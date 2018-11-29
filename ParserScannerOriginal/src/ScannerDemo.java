22546650137996ec7727ebf1bed1bd23eea57946
David Bochan <david.bochan@gmail.com>
2018-11-28 21:42:06 -0500

    Add Scanner implementation

---
+++ ParserScannerOriginal/src/ScannerDemo.java
@@ -0,0 +1,25 @@
+/**
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "./src/res/tests.jay";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+		TokenStream ts = new TokenStream(file1);
+
+		System.out.println(file1);
+
+		while (!ts.isEndofFile()) {
+			// System.out.println("Scanning..");
+			Token t = ts.nextToken();
+			System.out.println(counter + " - " + t.toString());
+			counter++;
+		}
+
+		System.out.println("Finished execution.");
+	}
+}
